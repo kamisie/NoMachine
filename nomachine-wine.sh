da2aa207c9f1db8f078a6cc0acc9afeffe11daae
kamisie <85386941+kamisie@users.noreply.github.com>
2022-04-29 10:01:57 +0700

    Update nomachine-wine.sh

--- nomachine-wine.sh
+++ nomachine-wine.sh
@@ -39,10 +39,11 @@ docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWOR
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo "NewURL: curl -sLkO https://raw.githack.com/kamisie/NoMachine/main/nomachine-wine.sh ; bash nomachine-wine.sh"
+echo "NOTE: RDP ( UBER JAM TAYANG )"
 echo Done! NoMachine Information:
 echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
-echo User: sukorejo
+echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
